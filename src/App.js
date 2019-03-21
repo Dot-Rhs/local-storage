404a5fd7c91c13c793ee8dec6eddf3bec02bc6aa
unknown <rhysh1986@gmail.com>
2019-03-21 16:50:07 +0000

    saves state via cookies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
+  componentDidMount() {
+    const counter = JSON.parse(localStorage.getItem("counter")) || 0; //without JSON.parse, num returns as a string
+    //you could also set it in state above "counter: JSON.parse(loc... etc"
+    this.setState(() => ({ counter }));
+  }
+
+  increment = () => {
+    this.setState(state => ({ counter: state.counter + 1 }));
+  };
+
+  save = () => {
+    localStorage.setItem("counter", this.state.counter);
+  };
+
+  reset = () => {
+    localStorage.clear(); // localStorage.removeItem("counter")
+    this.setState(state => ({ counter: (state.counter = 0) }));
+  };
+
   render() {
+    const { counter } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div>{counter}</div>
+        <button onClick={this.increment}>+</button>
+        <button onClick={this.save}>save</button>
+        <button onClick={this.reset}>Reset</button>
       </div>
     );
   }
